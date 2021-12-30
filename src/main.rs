00d2292f8a299c66ef388e1b3a390f08fe0348db
horus645 <leonardo.fae44@gmail.com>
2021-12-30 17:40:53 -0300

    Created img and kill commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use fork;
+use nix::{sys::signal, unistd::Pid};
 use structopt::StructOpt;
 mod daemon;
+
+const PID_FILE: &str = "/tmp/fswww/pid";
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -18,6 +21,9 @@ enum Fswww {
 
     ///Kills the daemon
     Kill,
+
+    /// Send an img for the daemon to display
+    Img { path: String },
 }
 
 fn main() {
@@ -35,7 +41,40 @@ fn main() {
             }
         }
         Fswww::Kill => kill(),
+        Fswww::Img { path } => send_img(&path),
     }
 }
 
-fn kill() {}
+fn send_img(path: &str) {
+    let pid = get_daemon_pid();
+    let mut img_path = path.to_string();
+    img_path.push('\n');
+    std::fs::write("/tmp/fswww/in", img_path)
+        .expect("Couldn't write to /tmp/fswww/in. Did you delete the file?");
+
+    signal::kill(Pid::from_raw(pid), signal::SIGUSR1).expect("Failed to send signal.");
+}
+
+fn kill() {
+    let pid = get_daemon_pid();
+
+    signal::kill(Pid::from_raw(pid), signal::SIGKILL).expect("Failed to kill daemon...");
+
+    std::fs::remove_dir_all("/tmp/fswww").expect("Failed to remove /tmp/fswww directory.");
+
+    println!("Successfully killed fswww daemon and removed /tmp/fswww directory!");
+}
+
+fn get_daemon_pid() -> i32 {
+    let pid_file_path = std::path::Path::new(PID_FILE);
+    if !pid_file_path.exists() {
+        panic!(
+            "pid file {} doesn't exist. Are you sure the daemon is running?",
+            PID_FILE
+        );
+    }
+    std::fs::read_to_string(pid_file_path)
+        .expect("Failed to read pid file")
+        .parse()
+        .unwrap()
+}
