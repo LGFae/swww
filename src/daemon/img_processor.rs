8484c82108e317bcb73122e9792c4c878a83b490
horus645 <leonardo.fae44@gmail.com>
2022-01-01 22:18:56 -0300

    Checking if img sent is a gif

--- src/daemon/img_processor.rs
+++ src/daemon/img_processor.rs
@@ -1,4 +1,5 @@
 use image::{self, imageops::FilterType, GenericImageView};
+use image::{AnimationDecoder, ImageFormat};
 use log::{debug, info};
 
 use smithay_client_toolkit::reexports::calloop::channel::Channel;
@@ -38,23 +39,47 @@ fn handle_msg(
     outputs: Vec<String>,
     dimensions: (u32, u32),
     filter: FilterType,
-    img: PathBuf,
+    path: PathBuf,
 ) {
     let (width, height) = dimensions;
-    let img = img_try_open_and_resize(&img, width, height, filter);
-    info!("Img is ready!");
-    sender.send((outputs, img)).unwrap();
+
+    //We check if we can open and read the image before sending it, so these should never fail
+    let img = image::io::Reader::open(&path)
+        .expect("Failed to open image, though this should be impossible...");
+    let img_bytes;
+    match img.format() {
+        Some(ImageFormat::Gif) => {
+            let gif = image::codecs::gif::GifDecoder::new(img.into_inner())
+                .expect("Failed to read gif. This should be impossible");
+            let mut frames = gif.into_frames().collect_frames().unwrap();
+            if frames.len() > 1 {
+                return;
+            } else {
+                img_bytes = img_try_open_and_resize(
+                    image::DynamicImage::ImageRgba8(frames.pop().unwrap().into_buffer()),
+                    width,
+                    height,
+                    filter,
+                );
+            }
+        }
+        None => unreachable!("Unsupported format. This also should be impossible..."),
+        _ => img_bytes = img_try_open_and_resize(img.decode().unwrap(), width, height, filter),
+    };
+
+    debug!(
+        "Sending message back from processor. {:?}, {}x{}",
+        path, width, height
+    );
+    sender.send((outputs, img_bytes)).unwrap();
 }
 
 fn img_try_open_and_resize(
-    img_path: &Path,
+    img: image::DynamicImage,
     width: u32,
     height: u32,
     filter: FilterType,
 ) -> Vec<u8> {
-    //We check if image::open works before sending it, so it should never fail
-    let img =
-        image::open(img_path).expect("Failed to open image, though this should be impossible...");
     let img_dimensions = img.dimensions();
     debug!("Output dimensions: width: {} height: {}", width, height);
     debug!(
@@ -71,9 +96,5 @@ fn img_try_open_and_resize(
 
     // The ARGB is 'little endian', so here we must  put the order
     // of bytes 'in reverse', so it needs to be BGRA.
-    debug!(
-        "Sending message back from processor: {:?}, {}x{}",
-        img_path, width, height
-    );
     resized_img.into_bgra8().into_raw()
 }
