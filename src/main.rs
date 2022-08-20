dd0e947a562947f1d5d9b69b3c1428407097d6ab
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2022-08-20 14:26:21 -0300

    Correct regression with init && img.
    Regression caused by 2ccebee (Merge pull request #19 from
    Horus645/better-error-msg-when-daemon-is-not-running, 2022-08-17)

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ fn connect_to_socket(tries: u8, interval: u64) -> Result<UnixStream, String> {
     //Make sure we try at least once
     let tries = if tries == 0 { 1 } else { tries };
     let path = communication::get_socket_path();
-    let mut error = String::new();
+    let mut error = None;
     for _ in 0..tries {
         match UnixStream::connect(&path) {
             Ok(socket) => {
@@ -83,16 +83,14 @@ fn connect_to_socket(tries: u8, interval: u64) -> Result<UnixStream, String> {
                 }
                 return Ok(socket);
             }
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::NotFound {
-                    return Err(
-                        "Socket file not found. Are you sure the daemon is running?".to_string()
-                    );
-                }
-                error = e.to_string();
-            }
+            Err(e) => error = Some(e),
         }
         std::thread::sleep(Duration::from_millis(interval));
     }
-    Err("Failed to connect to socket: ".to_string() + &error)
+    let error = error.unwrap();
+    if error.kind() == std::io::ErrorKind::NotFound {
+        return Err("Socket file not found. Are you sure the daemon is running?".to_string());
+    }
+
+    Err(format!("Failed to connect to socket: {}", error))
 }
