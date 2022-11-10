08f3b2fd25c713c2f2e9a510d90b807b9b23d4ae
Thomas Croft <103956335+thomascft@users.noreply.github.com>
2022-11-10 14:03:47 -0700

    Added home-manager module

--- flake.nix
+++ flake.nix
@@ -20,19 +20,62 @@
         buildInputs = with pkgs; [ libxkbcommon lz4 ];
       };
 
-      apps.x86_64-linux.default = {
-        type = "app";
-	program = "${self.packages.x86_64-linux.swww}/bin/swww";
-      };
-
       overlays = {
-        swww = _: prev: {
-          swww = self.packages.x86_64-linux.swww;
-        };
+        swww = _: prev: { swww = self.packages.x86_64-linux.swww; };
         default = self.overlays.swww;
       };
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ pkg-config libxkbcommon lz4 ];
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkg-config
+          libxkbcommon
+          lz4
+          rustc
+          cargo
+          rust-analyzer
+        ];
       };
+
+      homeManagerModules.default = { config, lib, pkgs, ... }:
+        let cfg = config.programs.swww;
+        in {
+          options.programs.swww = {
+            enable = lib.mkEnableOption
+              "swww, a solution to your wayland wallpaper woes";
+            package = lib.mkOption {
+              type = lib.type.package;
+              default = self.packages.x86_64-linux.default;
+            };
+            systemd = {
+              enable = lib.mkEnableOption "Enable systemd integration";
+              target = lib.mkOption {
+                type = lib.types.str;
+                default = "graphical-session.target";
+              };
+            };
+          };
+          config = lib.mkIf cfg.enable (lib.mkMerge [
+            { home.packages = lib.optional (cfg.package != null) cfg.package; }
+
+            (lib.mkIf cfg.systemd.enable {
+              systemd.user.services.swww = {
+                Unit = {
+                  Description =
+                    "swww, a solution to your wayland wallpaper woes";
+                  Documentation = "https://github.com/Hourus645/swww";
+                  PartOf = [ "graphical-session.target" ];
+                  After = [ "graphical-session.target" ];
+                };
+
+                Service = {
+                  ExecStart = "${cfg.package}/bin/swww init --no-daemon";
+                  ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+                  Restart = "on-failure";
+                  KillMode = "mixed";
+                };
+                Install = { WantedBy = [ cfg.systemd.target ]; };
+              };
+            })
+          ]);
+        };
     };
 }
