LGFae/swww

commit 29e268370656359f72b97ff3e81c1948e610ab54

don't try to animate inanimate gifs

utils/src/communication.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::{
     fmt,
+    io::{BufReader, BufWriter},
     os::unix::net::UnixStream,
     path::{Path, PathBuf},
-    time::Duration, io::{BufWriter, BufReader},
+    time::Duration,
 };
 
 use crate::comp_decomp::BitPack;
@@ -105,6 +106,13 @@ pub enum Request {
 
 impl Request {
     pub fn send(&mut self, stream: &UnixStream) -> Result<(), String> {
+        if let Request::Animation(vec) = self {
+            // don't send the animation request if we only have one frame
+            if vec.len() == 1 {
+                return Ok(());
+            }
+        }
+
         let writer = BufWriter::new(stream);
         match bincode::serialize_into(writer, self) {
             Ok(()) => Ok(()),

commit 86b674e118f6ebb896065a3624b33cc4b9082336

fix insane problem introduced by 29e2683

daemon/src/processor/mod.rs
@@ -92,6 +92,10 @@ impl Processor {
                     std::thread::yield_now();
                 }
                 let mut now = std::time::Instant::now();
+                /* We only need to animate if we have > 1 frame */
+                if animation.animation.len() == 1 {
+                    return;
+                }
                 for (frame, duration) in animation.animation.iter().cycle() {
                     let frame = frame.ready(output_size);
                     let timeout = duration.saturating_sub(now.elapsed());

utils/src/communication.rs
@@ -106,13 +106,6 @@ pub enum Request {
 
 impl Request {
     pub fn send(&mut self, stream: &UnixStream) -> Result<(), String> {
-        if let Request::Animation(vec) = self {
-            // don't send the animation request if we only have one frame
-            if vec.len() == 1 {
-                return Ok(());
-            }
-        }
-
         let writer = BufWriter::new(stream);
         match bincode::serialize_into(writer, self) {
             Ok(()) => Ok(()),