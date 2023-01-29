5a3dbfa340f1384f9dcc3df2c18baaa7af2963f1
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2023-01-29 11:47:42 -0300

    nuke `fix_zsh_completion.sh`
    It seems `clap-complete` now generates correct completion scripts; we no
    longer have to fix it manually.

--- build.rs
+++ build.rs
@@ -15,16 +15,7 @@ fn main() -> Result<(), Error> {
     let shells = [Shell::Bash, Shell::Zsh, Shell::Fish, Shell::Elvish];
     for shell in shells {
         let comp_file = generate_to(shell, &mut app, APP_NAME, &outdir)?;
-        println!(
-            "cargo:warning=generated shell completion file: {comp_file:?}"
-        );
-    }
-    if let Err(e) = std::process::Command::new("./fix_zsh_completion.sh").output() {
-        println!(
-            "cargo:warning=FAILED TO RUN fix_zsh_completion.sh SCRIPT: {e}"
-        );
-    } else {
-        println!("cargo:warning=ran fix_zsh_completion.sh script.");
+        println!("cargo:warning=generated shell completion file: {comp_file:?}");
     }
     Ok(())
 }
