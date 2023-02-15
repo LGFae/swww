672e2b2058dc254d4041d810b4658f7b4bbf51b0
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2023-02-15 12:58:04 -0300

    document doc/gen.sh script

--- doc/gen.sh
+++ doc/gen.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# This script generates man pages in a `doc/generated` directory. In order to
+# install these, you need to move the man pages to the appropriate location in
+# your system. You should be able to figure out the correct directories by
+# running `manpath`.
+#
+# Package Maintainers: please consult your distribution's specific documentation
+# to adapt to whatever idiosyncrasies it may have.
+
 set -e
 
 DIR=$(dirname "$0")
