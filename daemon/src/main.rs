1711140520b4eb735e0f52f86a825e54e2d368ed
b3nj5m1n <b3nj4m1n@gmx.net>
2023-04-19 08:51:35 +0200

    Send status update to systemd when daemon has initialized

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -425,6 +425,11 @@ fn main_loop(
     }
 
     info!("Initialization succeeded! Starting main loop...");
+    if let Ok(true) = sd_notify::booted() {
+        if let Err(e) = sd_notify::notify(true, &[sd_notify::NotifyState::Ready]) {
+            return Err(e.to_string());
+        }
+    }
     let mut loop_signal = event_loop.get_signal();
     if let Err(e) = event_loop.run(None, &mut loop_signal, |_| {
         {
