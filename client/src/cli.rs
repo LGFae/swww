3cdf15bae9249b938cca77ee6eb2ac203cbcc395
rexept <io@etriadi.com>
2024-10-25 21:29:40 -0600

    more concise definition

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -212,7 +212,7 @@ pub enum ResizeStrategy {
     Crop,
     /// Resize the image to fit inside the screen, preserving the original aspect ratio
     Fit,
-    /// Resize the image to stretch across the screen
+    /// Resize the image to fit inside the screen, without preserving the original aspect ratio
     Stretch,
 }
 
